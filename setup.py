c323cc53aef73167659a5a7f52a22dba92439aed
Kaysman <42100908+Kaysman@users.noreply.github.com>
2020-08-13 14:51:24 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+setup(
+    name='gemballs',
+    packages=['gemballs'],
+    version='0.1',
+    license='https://github.com/Kaysman/gemballs/blob/master/LICENSE.md',
+    description='A powerful machine learning algorithm for binary classification',
+    author='Omar Younis',
+    author_email='omar.younis98@gmail.com',
+    url='https://github.com/Kaysman/gemballs',
+    download_url='https://github.com/Kaysman/gemballs/archive/0.1.tar.gz',
+    keywords=['MACHINE LEARNING', 'GEM-balls', 'CLASSIFICATION'],
+    install_requires=[
+        'numpy',
+        'multiprocessing',
+        'os',
+        'scipy'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Topic :: Software Development :: Build Tools',
+        'License :: Other/Proprietary License',
+        'Programming Language :: Python :: 3',
+    ],
+)
