6cd2916ff323189c60bb1c9fcb3428cedd87fa20
Kaysman <42100908+Kaysman@users.noreply.github.com>
2020-08-13 16:21:47 +0200

    info about package

---
+++ gemballs/__init__.py
@@ -0,0 +1,5 @@
+from .gemballspollingclassifier import GEMBallsPollingClassifier
+from .gemballsclassifier_kdtree import GEMBallsKDTree
+from .gemballsclassifier import GEMBallsClassifier
+
+__all__ = ['GEMBallsClassifier', 'GEMBallsKDTree', 'GEMBallsPollingClassifier']
